c7ac3014e36d9588e225dd26375229f7512c378f
Rachel <rmoshier@gmail.com>
2014-12-19 15:26:24 -0800

    figuring out ups stuff

--- app/controllers/shipping_logs_controller.rb
+++ app/controllers/shipping_logs_controller.rb
@@ -23,4 +23,26 @@ include ActiveMerchant::Shipping
     render json: @ups_rates.as_json
   end
 
+  def fedex_shipping
+    package = Package.new(100, nil)
+    origin = Location.new(      :country => 'US',
+    :state => 'WA',
+    :city => 'Seattle',
+    :zip => '98112')
+
+    destination = Location.new( :country => 'US',
+    :state => 'WA',
+    :city => 'Seattle',
+    :postal_code => '98101')
+
+    fedex = FedEx.new(:login => ENV["FEDEX_LOGIN"], :password => ENV["FEDEX_PASSWORD"], key: ENV["FEDEX_KEY"], account: ENV["FEDEX_ACCOUNT"])
+    # tracking_info = fedex.find_tracking_info('tracking-number', :carrier_code => 'fedex_ground') # Ground package
+    response = fedex.find_rates(origin, destination, package)
+
+    @fedex_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+
+    render json: @fedex_rates.as_json
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   get '/ups',    to: 'shipping_logs#ups_shipping'
+  get '/fedex',  to: 'shipping_logs#fedex_shipping'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
